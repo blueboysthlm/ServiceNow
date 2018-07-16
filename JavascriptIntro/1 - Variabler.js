db3cff9c1043a7401a6f2a42a6e28032e2390f17
Oscar Olsson <happybits@gmail.com>
2018-07-16 08:23:32 +0200

    no message

---
+++ JavascriptIntro/1 - Variabler.js
@@ -0,0 +1,55 @@
+﻿
+// Skriv ut "Jag heter XXXX"
+
+// Skapa variabel och sätt det till 35. 
+// Meddela värdet av siffran (med hjäp av "console.log")
+
+// Skapa en variabel "x" med värdet 12
+// Skapa en variabel "y" med värdet 6
+// Skapa en variabel "z" som summerar talen
+// Skriv ut svaret "Svar: 18"
+
+// Modifera koden du skapat sist och se vilka effekter/felmeddelanden som dyker upp
+// Experimentera!
+
+// Skapa en variabel "x" med värdet 77
+// Skapa en variabel "y" med värdet 44
+// Ändra värdet av "x" till 55
+// Skapa en variabel "z" som summerar talen
+// Skriv ut summan
+
+// Samma som sist men låter x vara en konstant
+// Vilket felmeddelande dyker upp?
+
+// Modifera koden du skapat sist och se vilka effekter/felmeddelanden som dyker upp
+// Experimentera!
+
+// Skapa en variabel "nummer1" och sätt den till strängen "77" 
+// Skapa en variabel "nummer2" och sätt den till strängen "50"
+// Skriv ut nummer1+nummer2 (ska ge 7750)
+
+// Skapa en variabel "nummer1" och sätt den till strängen "77" 
+// Skapa en variabel "nummer2" och sätt den till 50
+// Använd parseInt för att omvandla "nummer1" till siffran 77
+// Skriv ut nummer1+nummer2 (ska ge 127)
+
+
+// Skapa variabel nummer med värdet 42. 
+// Skriv ut "Mitt favorittal är 42" (om det är 42).
+
+// Använd "snedfnutt" för att lösa problemet ovan
+
+// Skapa en variabel "middag" och sätt den till 'broccoli'
+// Skapa en variabel "Middag" och sätt den till 'pasta'
+// Meddela värdet av "middag"
+
+// Skapa en variabel x med värdet 12
+// Skapa en variabel y med värdet true
+// Skapa en variabel z med värdet 'måndag'
+// Skapa en variabel w utan att sätta något värde
+// Skriv ut typen av dessa fyra variabler mha "typeof"
+
+// Skapa en variabel "x". Sätt värdet till "parseInt('oscar')"
+// Skriv ut x (ska ge "NaN")
+// Använd "isNaN" fär att kolla om x är ett icke-nummer. Sätt y till det värdet (y kommer få värdet true)
+// Skriv ut y
---
+++ Referenser/Referenser - javascript.md
@@ -0,0 +1,40 @@
+## Installera node
+
+https://nodejs.org/
+
+## Installera Visual Studio Code
+
+https://code.visualstudio.com/download
+
+Alternativt Sublime Text
+
+https://www.sublimetext.com/3
+
+## JavaScript Fundamentals For Beginners (2016)
+
+1h och 8min. Rekommenderas. 
+
+https://www.youtube.com/watch?v=vEROU2XtPR8
+
+
+## Javascript fundementals for absolute beginners (2018)
+
+7.5h. Rekommenderas. Kolla upp till kapitel 13.
+
+https://mva.microsoft.com/en-US/training-courses/javascript-fundamentals-for-absolute-beginners-14194?l=svAqFyUxE_6300115881
+
+## The Modern JavaScript Tutorial
+
+https://javascript.info/
+
+## Javascript
+
+https://www.w3schools.com/jS/
+
+## HTML 
+
+https://www.w3schools.com/html/
+
+## CSS
+
+https://www.w3schools.com/css/
